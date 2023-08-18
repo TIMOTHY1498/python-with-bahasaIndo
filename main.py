6c12b8342cf5cab54008726ab0b8d8f66d7ef930
Timothy B <74752830+TIMOTHY1498@users.noreply.github.com>
2023-08-18 14:47:58 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import pythonindo
+
+bruh = pythonindo.cetak("halo NAMA SAYA JOHN")
+contoh = input("yangbenr [y/n] >>>")
+
+pythonindo.selamabenarlakukan()
+    if contoh == "y":
+        pythonindo.cetak("halo")
+        pythonindo.tidur(1)
+        pythonindo.cetak("Selamat Turu!")
+    else:
+        pythonindo.cetak("Pilh yg berer, bgst!")
+        pythonindo.cetak("pintel;")
---
+++ pythonindo.py
@@ -0,0 +1,11 @@
+# Python but with Bahasa Indonesia for writing code
+# v.0.0.1 for python311
+
+# Function
+
+def cetak(name):
+    print(name)
+    
+def tidur(timesleep):
+    import time
+    time.sleep(timesleep)
